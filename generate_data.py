945e48a904420a45f5fd09193a78df6602abfd48
MrTurtleW <yqgffm@gmail.com>
2017-08-26 14:03:19 +0800

    modify generate_data.py

--- generate_data.py
+++ generate_data.py
@@ -15,13 +15,12 @@ def insert_data():
     collection = db['goods_sales']
 
     max_goods_number = 1000
-    max_sales_number = 1000
+    max_sales_number = 10000
     start_dt = "2014-01-01 00:00:00"
 
-    timeArray = time.strptime(start_dt, '%Y-%m-%d %H:%M:%S')
-    start_timestamp = time.mktime(timeArray)
+    
 
-    for goods_id in xrange(max_goods_number):
+    for goods_id in xrange(max_goods_number*0, max_goods_number*1):
 
         # 价格
         # 随机生成，20-30次变一次
@@ -29,32 +28,32 @@ def insert_data():
         current_price_times = 0
         current_price = float('%.2f' % random.uniform(100, 150))
 
-        # 日期
-        current_timestamp = start_timestamp
+        # time
+        sold_time = datetime.datetime.strptime(start_dt, "%Y-%m-%d %H:%M:%S")
 
         for sales_count in xrange(max_sales_number):
 
-            # 计算商品售出时间，0-10秒随机递增
-            current_timestamp += random.randint(0, 10)
-            sold_time = datetime.datetime.fromtimestamp(current_timestamp)
-            print sold_time
+            # 计算商品售出时间，随机递增
+            seconds = random.randint(1000, 2000)
+            sold_time = sold_time + datetime.timedelta(seconds = seconds)
+            
             # 商品售出价格
             if current_price_times >= max_price_times:
                 max_price_times = random.randint(20, 30)
                 current_price_times = 0
                 current_price = float('%.2f' % random.uniform(100, 150))
-                print current_price
 
             current_price_times = current_price_times + 1
 
             # 写入数据库
             good_sales_record = {'goods_id': goods_id,
-                                 'sold_times': sold_time, 'sold_price': current_price}
+                                 'sold_time': sold_time, 'sold_price': current_price}
             print good_sales_record
-            collection.save(good_sales_record)
+            # collection.save(good_sales_record)
+            db.goods_sales.insert(good_sales_record)
 
     end_time = datetime.datetime.now()
-    print (end_time - start_time).seconds
+    print end_time - start_time
 
 if __name__ == '__main__':
     insert_data()
